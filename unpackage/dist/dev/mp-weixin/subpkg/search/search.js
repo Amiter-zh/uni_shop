23d600e3d42fe5346f09c883a66a899b1a3773e9
Aimer <15622141125@163.com>
2023-03-31 07:59:58 +0800

    main update

---
+++ unpackage/dist/dev/mp-weixin/subpkg/search/search.js
@@ -0,0 +1,36 @@
+(global["webpackJsonp"] = global["webpackJsonp"] || []).push([["subpkg/search/search"],{
+
+/***/ 77:
+/*!************************************************************************************************!*\
+  !*** D:/class sign/wechat-app-travel/tes/uni_shop/main.js?{"page":"subpkg%2Fsearch%2Fsearch"} ***!
+  \************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function(wx, createPage) {
+
+var _interopRequireDefault = __webpack_require__(/*! @babel/runtime/helpers/interopRequireDefault */ 4);
+__webpack_require__(/*! uni-pages */ 26);
+var _vue = _interopRequireDefault(__webpack_require__(/*! vue */ 25));
+var _search = _interopRequireDefault(__webpack_require__(/*! ./subpkg/search/search.vue */ 78));
+// @ts-ignore
+wx.__webpack_require_UNI_MP_PLUGIN__ = __webpack_require__;
+createPage(_search.default);
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./node_modules/@dcloudio/uni-mp-weixin/dist/wx.js */ 1)["default"], __webpack_require__(/*! ./node_modules/@dcloudio/uni-mp-weixin/dist/index.js */ 2)["createPage"]))
+
+/***/ }),
+
+/***/ 78:
+/*!*****************************************************************************!*\
+  !*** D:/class sign/wechat-app-travel/tes/uni_shop/subpkg/search/search.vue ***!
+  \*****************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/@dcloudio/webpack-uni-mp-loader/lib/style.js):\nError: ENOENT: no such file or directory, open 'D:\\class sign\\wechat-app-travel\\tes\\uni_shop\\subpkg\\search\\search.vue'");
+
+/***/ })
+
+},[[77,"common/runtime","common/vendor"]]]);
+//# sourceMappingURL=../../../.sourcemap/mp-weixin/subpkg/search/search.js.map
