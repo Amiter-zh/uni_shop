42cdbe80cf7be0e0ec0bc148c204d9379cf1559f
Aimer <15622141125@163.com>
2023-04-05 01:33:00 +0800

    完成

--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,13 @@
 <template>
 	<view>
-		xxxx
+
+		<view class="good_list">
+			<view v-for="(item, i) in goodsList" :key="i" @click="gotoDetail(item)">
+				<my-goods :goods="item"></my-goods>
+			</view>
+		</view>
+
+
 	</view>
 </template>
 
@@ -8,8 +15,58 @@
 	export default {
 		data() {
 			return {
-				
+				queryObj: {
+					query: '',
+					cid: '',
+					pagenum: 1,
+					pagesize: 10
+				},
+				goodsList: [],
+				total: 0,
+				isloading: false
 			};
+		},
+		onLoad(options) {
+			// console.log(options);
+			this.queryObj.query = options.query || ''
+			this.queryObj.cid = options.cid || ''
+			// console.log(this.queryObj);
+			this.getgoodsL()
+		},
+		methods: {
+			async getgoodsL(cb) {
+				this.isloading = true
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+				this.isloading = false
+				
+				cb && cb()
+				if (res.meta.status !== 200) return uni.$showMSg()
+				// this.goodsList = res.message.goods
+				this.goodsList = [...this.goodsList, ...res.message.goods]
+				this.total = res.message.total
+				// console.log(res);
+			},
+			gotoDetail(item){
+				uni.navigateTo({
+					url:'/subpkg/goods_detail/goods_detail?goods_id=' + item.goods_id
+				})
+			}
+		},
+		onReachBottom() {
+			if(this.queryObj.pagenum * this.queryObj.total >= this.total) return uni.$showMSg('加载完毕')
+			if(this.isloading) return
+			this.queryObj.pagenum += 1
+			this.getgoodsL()
+		},
+		onPullDownRefresh() {
+			this.queryObj.pagenum = 1
+			this.total = 0
+			this.isloading = false
+			this.goodsList = []
+			// 发送请求
+			this.getgoodsL(() => uni.stopPullDownRefresh())
 		}
 	}
 </script>
